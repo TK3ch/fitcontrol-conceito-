e38b8bd22caf918766e19731b7ad99ce24315f0e
DevrSoares <matheus.soares@nautasoft.com.br>
2025-11-17 11:46:38 -0300

    Atual_sql

--- SQL/criar_banco.sql
+++ SQL/criar_banco.sql
@@ -1,35 +1,46 @@
-CREATE DATABASE IF NOT EXISTS sistema_academia;
-USE sistema_academia;
 
-CREATE TABLE IF NOT EXISTS Academias (
-    Id INT AUTO_INCREMENT PRIMARY KEY,
-    Nome VARCHAR(100) NOT NULL,
-    Endereco VARCHAR(200),
-    Telefone VARCHAR(20)
+CREATE DATABASE academia;
+USE academia;
+
+
+CREATE TABLE Planos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    Nome_plano VARCHAR(80) NOT NULL,
+    Valor_plano DECIMAL(10,2) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Alunos (
-    Id INT AUTO_INCREMENT PRIMARY KEY,
-    Nome VARCHAR(100) NOT NULL,
-    EstaPagando BOOLEAN NOT NULL,
-    AcademiaId INT,
-    FOREIGN KEY (AcademiaId) REFERENCES Academias(Id)
+
+CREATE TABLE Unidades (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    Endereco VARCHAR(250) NOT NULL,
+    Telefone VARCHAR(20),
+    cnpj_unidade VARCHAR(20)
 );
 
-CREATE TABLE IF NOT EXISTS Materias (
-    Id INT AUTO_INCREMENT PRIMARY KEY,
-    Nome VARCHAR(100) NOT NULL,
-    Custo DECIMAL(10,2) NOT NULL,
-    AcademiaId INT,
-    FOREIGN KEY (AcademiaId) REFERENCES Academias(Id)
+
+CREATE TABLE Alunos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    Nome_al VARCHAR(85) NOT NULL,
+    CPF_al VARCHAR(11) NOT NULL,
+    Idade_al VARCHAR(2),
+    Genero_al VARCHAR(15),
+    Numero_matricula VARCHAR(25),
+    plano_al INT,
+    unidade INT,
+
+    FOREIGN KEY (plano_al) REFERENCES Planos(id),
+    FOREIGN KEY (unidade) REFERENCES Unidades(id)
 );
 
-CREATE TABLE IF NOT EXISTS Pagamentos (
-    Id INT AUTO_INCREMENT PRIMARY KEY,
-    AlunoId INT,
-    Valor DECIMAL(10,2),
-    DataPagamento DATETIME,
-    AcademiaId INT,
-    FOREIGN KEY (AlunoId) REFERENCES Alunos(Id),
-    FOREIGN KEY (AcademiaId) REFERENCES Academias(Id)
+CREATE TABLE Pagtos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_unidade INT NOT NULL,
+    Cod_pagto VARCHAR(80),
+    data_pagto DATE,
+    plano INT,
+    aluno_pag INT,
+
+    FOREIGN KEY (id_unidade) REFERENCES Unidades(id),
+    FOREIGN KEY (plano) REFERENCES Planos(id),
+    FOREIGN KEY (aluno_pag) REFERENCES Alunos(id)
 );
